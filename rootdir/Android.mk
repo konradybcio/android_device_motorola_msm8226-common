cdb159e856e69a9ca7bd89f1a19f48c65c00317b
Konrad Dybcio <konradybcio@gmail.com>
2020-02-29 20:16:51 +0100

    Add a script to enable Wi-Fi on boot
    Removes the need to run the usual documented commands
    to get Wi-Fi working. Shamelessly copied from fp2.
    
    Change-Id: I0173370542fcd75775c684a3595a39ddccddbe18

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,3 +82,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# WLAN init script
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init_wlan_bt.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init_wlan_bt.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
